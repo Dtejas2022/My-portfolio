1e8e74b1a7eac7afd10fa69745f7d4e06e17ac0c
Tejas Dotkar <106228821+Dtejas2022@users.noreply.github.com>
2024-08-12 07:22:56 +0000

    navbar added

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header';
-
+import Homepage from './pages/Homepage';
 function App() {
   return (
     <div>
-      <Header/>
+      <Homepage/>
     </div>
   );
 }
--- my-portfolio/src/components/Header.jsx
+++ my-portfolio/src/components/Header.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div>
-      <div>Tejas dotkar</div>
+    <div className='w-full h-14'>
+      <nav class="w-full h-14 bg-slate-400 flex justify-between items-center px-4 md:px-4">
+        <div class="font-bold text-lg bg-red-600 text-white  px-2 py-2 rounded-xl cursor-pointer">Tejas Dotkar</div>
+        <ul class="flex text-black font-bold p-5">
+            <li class="mx-[10px] cursor-pointer p-4 hover:bg-red-500 hover:text-white">Home</li>
+            <li class="mx-[10px] cursor-pointer p-4 hover:bg-red-500 hover:text-white">About</li>
+            <li class="mx-[10px] cursor-pointer p-4 hover:bg-red-500 hover:text-white">Projects</li>
+            <li class="mx-[10px] cursor-pointer p-4 hover:bg-red-500 hover:text-white">Achievements</li>
+            <li class="mx-[10px] cursor-pointer p-4 hover:bg-red-500 hover:text-white">Contact</li>
+        </ul>
+    </nav>
     </div>
   )
 }
