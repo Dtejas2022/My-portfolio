5f3dc68834a64fe327aa9be1a6af960eb65d310b
Tejas Dotkar <106228821+Dtejas2022@users.noreply.github.com>
2024-08-12 08:05:21 +0000

    trying

---
+++ my-portfolio/src/components/Main.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function Main() {
+  return (
+    <div>
+      <div className=' py-4 bg-black'>
+        <h1 className='text-center font-bold text-3xl text-white'>Hi Everyone !!</h1>
+        <p className='text-white text-center font-bold text-xl'>I am Tejas Dotkar</p>
+      </div>
+      <div className='bg-white p-1'> </div>
+
+    <div className='flex h-96'>
+      <div className='w-1/2 m-5'>
+        <img className='h-[100%] w-96 center' src="DSC_0140.JPG" alt="" srcset="" />
+      </div>
+      <div className='w-1/2'>
+        <h1 className='text-3xl '>About Me</h1>
+        <div className='bg-red-400 p-[1px] w-[100px]'></div>
+        <div>
+            <p className='justify-normal'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa deleniti temporibus quos possimus, itaque perferendis quod magnam quam, iste dignissimos nobis repudiandae rem doloremque magni similique velit earum totam minus laborum molestiae numquam consequatur quidem sunt corporis. Voluptatibus explicabo cupiditate perferendis sapiente praesentium nam, rem hic quod repudiandae eligendi assumenda ex inventore quaerat similique incidunt consectetur autem alias, nisi perspiciatis reiciendis quidem voluptas! Officiis, quaerat pariatur. Illum eius minus aspernatur optio unde perferendis hic temporibus exercitationem quaerat odio ratione atque, autem fugit natus nisi rem quae reiciendis beatae. Sunt, a. Sapiente nesciunt libero similique repudiandae, debitis ducimus dolorem natus sequi.</p>
+        </div>
+      </div>
+    </div>
+
+    
+    </div>
+  )
+}
--- my-portfolio/src/pages/Homepage.jsx
+++ my-portfolio/src/pages/Homepage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Header from '../components/Header';
-
+import Main from '../components/Main';
 export default function Homepage() {
   return (
     <div>
       <Header/>
+      <Main/>
     </div>
   )
 }
